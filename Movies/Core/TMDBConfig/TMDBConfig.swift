21881c5563ca1fe1e7a86272916b882add50a0ae
Thiago Monteiro <thiagomonteiro.mont@gmail.com>
2026-05-13 16:44:44 -0300

    Removing unnecessary code

--- Movies/Core/TMDBConfig/TMDBConfig.swift
+++ Movies/Core/TMDBConfig/TMDBConfig.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 enum TMDBConfig {
-    static let baseURL = "https://api.themoviedb.org/3"
     static let imageBaseURL = "https://image.tmdb.org/t/p/w500"
     
     static func posterURL(_ path: String?) -> URL? {
